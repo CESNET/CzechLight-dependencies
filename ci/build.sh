9302fc0e7cdeaa8ad8feee3246f3a1ba58d284ec
Jan Kundrát <jkt@flaska.net>
2019-03-28 12:45:55 +0100

    Migrate from Catch to Doctest
    It's something that is quite close to Catch, but it is much, much faster
    to compile. Let's give it a try.
    
    There are no matchers and no proper support for data-driven testing, but
    I think that we can live without that. Cutting the build time of the
    tests by two thirds is worth it, IMHO.
    
    Change-Id: I52f5f9e4a93bd7c6be7d9978b5abcf9d58b0a125

--- ci/build.sh
+++ ci/build.sh
@@ -133,8 +133,8 @@ do_test_dep_cmake Netopeer2/keystored
 CMAKE_OPTIONS="${CMAKE_OPTIONS} -DPIDFILE_PREFIX=${PREFIX}/var-run" emerge_dep Netopeer2/server
 do_test_dep_cmake Netopeer2/server --timeout 30 -j${CI_PARALLEL_JOBS}
 
-emerge_dep Catch
-do_test_dep_cmake Catch -j${CI_PARALLEL_JOBS}
+emerge_dep doctest
+do_test_dep_cmake doctest -j${CI_PARALLEL_JOBS}
 
 # Trompeloeil is a magic snowflake because it attempts to download and build Catch and kcov when building in a debug mode...
 CMAKE_BUILD_TYPE=Release emerge_dep trompeloeil
