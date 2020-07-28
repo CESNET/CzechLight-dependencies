1c5fe355c753fa455f2bf12ede20445d75321a15
Václav Kubernát <kubernat@cesnet.cz>
2020-07-28 15:33:21 +0200

    Migrate to new NETCONF stack
    Change-Id: Ibb6932f11bed10eddb173bec0459c33e85072f02

--- ci/build.sh
+++ ci/build.sh
@@ -127,7 +127,7 @@ ctest --output-on-failure
 rm -rf ${RUN_TMP}/b-s-t
 popd
 
-emerge_dep libnetconf2
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DIGNORE_LIBSSH_VERSION=ON" emerge_dep libnetconf2
 # https://github.com/CESNET/libnetconf2/issues/153
 do_test_dep_cmake libnetconf2 -j${CI_PARALLEL_JOBS} -E test_io
 pushd ${BUILD_DIR}/libnetconf2
