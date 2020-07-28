d77daeed04a5f3862e815b7fc9e9621bf39035ac
Václav Kubernát <kubernat@cesnet.cz>
2020-07-28 15:33:21 +0200

    Migrate to new NETCONF stack
    Change-Id: Ibb6932f11bed10eddb173bec0459c33e85072f02

--- ci/build.sh
+++ ci/build.sh
@@ -127,7 +127,7 @@ ctest --output-on-failure
 rm -rf ${RUN_TMP}/b-s-t
 popd
 
-emerge_dep libnetconf2
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DIGNORE_LIBSSH_VERSION=ON" emerge_dep libnetconf2
 # https://github.com/CESNET/libnetconf2/issues/153
 do_test_dep_cmake libnetconf2 -j${CI_PARALLEL_JOBS} -E test_io
 pushd ${BUILD_DIR}/libnetconf2
@@ -135,10 +135,8 @@ ctest --output-on-failure -R test_io </dev/null
 popd
 
 mkdir ${BUILD_DIR}/Netopeer2
-emerge_dep Netopeer2/keystored
-do_test_dep_cmake Netopeer2/keystored
-CMAKE_OPTIONS="${CMAKE_OPTIONS} -DPIDFILE_PREFIX=${RUN_TMP}" emerge_dep Netopeer2/server
-do_test_dep_cmake Netopeer2/server --timeout 30 -j${CI_PARALLEL_JOBS}
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DPIDFILE_PREFIX=${RUN_TMP}" emerge_dep Netopeer2
+do_test_dep_cmake Netopeer2 --timeout 30 -j${CI_PARALLEL_JOBS}
 
 emerge_dep doctest
 do_test_dep_cmake doctest -j${CI_PARALLEL_JOBS}
